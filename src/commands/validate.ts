cb8c9cb30f0121553cdc68c07fdb2d539ca33d5c
Narongrit Unwerawattana <narongrit.kie@gmail.com>
2022-04-29 10:11:07 +0200

    feat: validate command

--- src/commands/validate.ts
+++ src/commands/validate.ts
@@ -1,28 +1,69 @@
 import { Command, Flags } from "@oclif/core";
-
+import Validator from "../validate";
+import { lstatSync, existsSync } from "fs";
+import { isAbsolute, join } from "path";
 export default class Validate extends Command {
-  static override description = "describe the command here";
+  static override description = "Validate jpm enabled package.json";
 
-  static override examples = ["<%= config.bin %> <%= command.id %>"];
+  static override examples = ["$ validate .", "$ validate ./package.json"];
+  static override usage = [
+    "validate [pacagke_json_path]",
+    "validate [directory]",
+  ];
 
   static override flags = {
-    // flag with a value (-n, --name=VALUE)
-    name: Flags.string({ char: "n", description: "name to print" }),
-    // flag with no value (-f, --force)
-    force: Flags.boolean({ char: "f" }),
+    verbose: Flags.boolean({ char: "v", description: "Verbose mode" }),
   };
 
-  static override args = [{ name: "file" }];
+  static override strict = false;
+
+  static override args = [
+    {
+      name: "path",
+      description:
+        "path to package.json file or directory that contains package.json",
+      required: true,
+    },
+  ];
 
   public async run(): Promise<void> {
     const { args, flags } = await this.parse(Validate);
 
-    const name = flags.name ?? "world";
-    this.log(
-      `hello ${name} from /home/nau/sdu/jpm-schema/src/commands/validate.ts`
-    );
-    if (args["file"] && flags.force) {
-      this.log(`you input --force and --file: ${args["file"]}`);
+    let path = args["path"];
+
+    // checks if argument is a absolute path or not
+    if (!isAbsolute(path)) {
+      path = join(process.cwd(), path);
+    }
+
+    // checks if argument is a directory
+    if (lstatSync(path).isDirectory()) {
+      path = join(path, "package.json");
+    }
+
+    if (flags.verbose) {
+      this.log("validating file: ", path);
+    }
+
+    if (!existsSync(path)) {
+      this.error(
+        "file not exists: " + flags.verbose
+          ? "lookup path " + path
+          : args["path"],
+        { exit: 1 }
+      );
+    }
+
+    try {
+      const res = Validator.validateFile(path);
+      if (res) {
+        this.log("true");
+      }
+    } catch (e) {
+      if (flags.verbose) {
+        this.error(e as Error);
+      }
+      this.error("false", { exit: 1 });
     }
   }
 }
